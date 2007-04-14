96423e868b6aaa2bcd87e8f3f64ee02d613535e8
Bernard Gray <bernard.gray@gmail.com>
2007-04-14 05:48:22 +0000

    [svn-inject] Installing original source of sidux-art-wallpaper

---
+++ debian/sidux-art-wallpaper-default.ex
@@ -0,0 +1,10 @@
+# Defaults for sidux-art-wallpaper initscript
+# sourced by /etc/init.d/sidux-art-wallpaper
+# installed at /etc/default/sidux-art-wallpaper by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
